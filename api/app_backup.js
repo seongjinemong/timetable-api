7b874a77b7af5267487818ee6dea60534b0d2d6f
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-10-18 00:53:04 +0900

    app test

--- api/app_backup.js
+++ api/app_backup.js
@@ -49,10 +49,25 @@ app.use(
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routing
 app.use("/", require("./routes"));
 
-app.all("*", (req, res) => res.status(404).json('route not defined'))
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
